3b4704ce784730c4c462f642dcdd20ca68dd7b48
naser92 <38062523+naser92@users.noreply.github.com>
2023-04-05 14:59:28 +0330

    check excel

--- core/ExcellData.py
+++ core/ExcellData.py
@@ -1,26 +1,101 @@
 import pandas as pd
 import numpy as np
 import xlsxwriter
+import openpyxl
 
 
 class ExcellData ():
     def __init__(self, path) -> None:
         self.path = path
+
     
     def getInvoice(self):
-        data = pd.read_excel(self.path,sheet_name="invoice", dtype=str)
-        data = data.replace(np.nan, '')
-        invoice = np.array(data)
-        return invoice
+        try:
+            data = pd.read_excel(self.path,sheet_name=self.name_column_invoice, dtype=str)
+            data = data.replace(np.nan, '')
+            invoice = np.array(data)
+            return invoice
+        except:
+            return []
 
     def getInvoiceItem(self):
-        data = pd.read_excel(self.path,sheet_name="itemInvoice", dtype=str)
-        data = data.replace(np.nan, '')
-        item = np.array(data)
-        return item
+        try:
+            data = pd.read_excel(self.path,sheet_name= self.name_column_invoiceItem, dtype=str)
+            data = data.replace(np.nan, '')
+            item = np.array(data)
+            return item
+        except:
+            return []
     
     def getPayment(self):
-        data = pd.read_excel(self.path,sheet_name="payment", dtype=str)
+        try:
+            data = pd.read_excel(self.path,sheet_name=self.name_column_payment, dtype=str)
+            data = data.replace(np.nan, '')
+            pay = np.array(data)
+            return pay
+        except:
+            return []
+        
+    
+        
+    def checkExcel(self, paternType:int):
+        data = openpyxl.load_workbook(self.path)
+        result = []
+        if len(data.sheetnames) > 3 or len(data.sheetnames) < 1:
+            return result
+        
+        index = 0
+        for sheet in data.sheetnames:
+            columns = data.get_sheet_by_name(sheet)
+            if index == 0 and paternType == 1:
+                if columns.max_column == 21 : 
+                    result.append(1) 
+                    self.name_column_invoice = sheet
+                else: result.append(0) 
+            elif index == 0 and paternType == 2:
+                if columns.max_column == 16 : 
+                    result.append(1) 
+                    self.name_column_invoice = sheet
+                else: result.append(0) 
+            elif index == 1 :
+                if columns.max_column == 20 : 
+                    result.append(1) 
+                    self.name_column_invoiceItem = sheet
+                else: result.append(0) 
+            elif index == 2 : 
+                if columns.max_column == 11 : 
+                    result.append(1) 
+                    self.name_column_payment =sheet
+                else: result.append(0) 
+            
+            index =+ 1
+        return result
+        # if len(data.sheetnames) == 3:
+        #     self.name_column_invoice = data.sheetnames[0]
+        #     self.name_column_invoiceItem = data.sheetnames[1]
+        #     self.name_column_payment = data.sheetnames[2]
+        #     if paternType == 1:
+        #         c = data.get_sheet_by_name(self.name_column_invoice)
+        #         print(c)  
+        # elif len(data.sheetnames) == 2:
+        #     self.name_column_invoice = data.sheetnames[0]
+        #     self.name_column_invoiceItem = data.sheetnames[1]
+        #     if paternType == 1:
+        #         c = data.get_sheet_by_name(self.name_column_invoice)
+        #         print(c.max_column)  
+        # else:
+        #     return False
+        # data._sheets[0]._parent.worksheets[0].max_column
+
+    def gettestdata(self):
+        wb = openpyxl.load_workbook(self.path)
+        data = pd.read_excel(self.path,sheet_name=None, dtype=str)
         data = data.replace(np.nan, '')
-        pay = np.array(data)
-        return pay
+        item = np.array(data)
+
+    
+
+if __name__ == "__main__":
+    ed = ExcellData("./data/sampel11.xlsx")
+    a = ed.checkExcel(2)
+    print (a)
--- frm_main.py
+++ frm_main.py
@@ -14,21 +14,25 @@ class MainForm:
         Label(self.base,bg="#d1ccc0",text="نرم افزار ارسال صورتحساب کیسان" ,width="500",height="4").pack()
         
         Label(self.base,text="نام کاربری").place(x=400,y=100)
-        self.txt_username = Text(self.base,width="15",height="1")
-        self.txt_username.place(x=250,y=100) 
+        self.txt_username = Text(self.base,width="30",height="1")
+        self.txt_username.place(x=100,y=100) 
 
         Label(self.base,text="کلمه عبور").place(x=400,y=130)
-        self.txt_password = Text(self.base,width="15",height="1")
-        self.txt_password.place(x=250,y=130)
+        self.txt_password = Text(self.base,width="30",height="1")
+        self.txt_password.place(x=100,y=130)
 
-        Button(self.base,text="تست لاگین",command=self.login).place(x=310,y=170)
+        Button(self.base,text="تست لاگین",command=self.loginTest).place(x=200,y=165)
+
+        self.numberPatern = ttk.Combobox(self.base, width=40, value=["انتخاب الگو صورتحساب","الگو 1 صورتحساب همراه با اطلاعات خریدار","الگو 2 صورتحساب بدون اطلاعات خریدار"])
+        self.numberPatern.current(0)
+        self.numberPatern.place(x=100,y=210)
         
         
-        Frame(self.base)
-        Button(self.base,text="انتخاب فایل",command=self.select_file).place(x=20,y=210)
+        
+        Button(self.base,text="انتخاب فایل",command=self.select_file).place(x=20,y=240)
 
         self.lbl_path = Label(self.base,bg="#ffffff",width="50",height="1")
-        self.lbl_path.place(x=100,y=213)
+        self.lbl_path.place(x=100,y=243)
 
 
         
@@ -36,7 +40,7 @@ class MainForm:
     def generateForm(self) -> None:
         self.base.mainloop()  
  
-    def login(self):
+    def loginTest(self):
         str_usename = self.txt_username.get("1.0", "end-1c")
         str_password = self.txt_password.get("1.0", "end-1c")
         token = api.getToken(str_usename,str_password)
@@ -51,10 +55,12 @@ class MainForm:
             ('Excel files', '*.xlsx'),
             ('All files', '*.*')
         )
-
-        self.path_file = fd.askopenfilename(title='Open a file',initialdir='/',filetypes=filetypes)
-        self.lbl_path.config(text=self.path_file)
-
+        patern = self.numberPatern.current()
+        if patern != 0:
+            self.path_file = fd.askopenfilename(title='Open a file',initialdir='/',filetypes=filetypes)
+            self.lbl_path.config(text=self.path_file)
+        else:
+            showwarning('انتخاب الگو',"نوع الگوی فاکتور را انتخاب کنید")
 
 
 if __name__ == "__main__":
