bd7bd4b6df442ed479c400c528de339aa3dabc39
naser92 <38062523+naser92@users.noreply.github.com>
2023-08-12 12:54:18 +0330

    update

--- update.py
+++ update.py
@@ -37,9 +37,24 @@ class Updater:
                     for data in responce.iter_content(block_size):
                         pbar.update(len(data))
                         f.write(data)
-
             return file_path
         return None
+    
+    def delete_file(self):
+        curen_dir = os.getcwd()
+        items = os.listdir(curen_dir)
+        try:
+            for item in items :
+                item_path = os.path.join(curen_dir,item)
+                if os.path.isfile(item_path):
+                    os.remove(item_path)
+                elif os.path.isdir(item_path):
+                    os.rmdir(item_path)
+            return True
+        except Exception as e:
+            print(f"An error removeFile: {e}")
+            return False
+
     
     def update_file (self):
         file = self.download_update()
